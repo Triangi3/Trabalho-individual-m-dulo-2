78b22130ef8a9073dc97d97f55731184bd182069
Ingrid <115805965+Triangi3@users.noreply.github.com>
2022-11-10 00:44:06 -0300

    Update trabalhomódulo2.js

--- trabalhomódulo2.js
+++ trabalhomódulo2.js
@@ -1,6 +1,14 @@
 let encode = document.querySelector("#encode");
 let decode = document.querySelector("#decode");
 let output = document.querySelector("textarea");
+let selecionar = document.querySelector('.select');
+let chaveCifra = document.getElementById('chaveCont');
+let radioBtn = document.querySelector('.radiobtn');
+let codificar = document.getElementById('codificar');
+let decodificar = document.getElementById('decodificar');
+let resultBtn = document.getElementById('resultbtn');
+let texto = document.getElementById('texto');
+
 
 encode.addEventListener("click", () => {
 output.value = btoa(output.value);
@@ -9,3 +17,14 @@ output.value = btoa(output.value);
 decode.addEventListener("click", () => {
 output.value = atob(output.value);
 });
+
+function select() {
+    select.addEventListener("click", function () {
+        if (select.value === "cifra") {
+            chaveCifra.style.display = "block";
+        } else {
+            chaveCifra.style.display = "none";
+        }
+    });
+}
+
